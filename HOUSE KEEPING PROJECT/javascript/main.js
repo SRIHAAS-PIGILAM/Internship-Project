8779a63ad20b43d1888705ef536d0a414af7f793
Srihaas <108931606+Srihaas-lap@users.noreply.github.com>
2022-10-23 15:12:45 +0530

    Add files via upload
    I have been changing the designs and working  and testing the project in all the aspects sometimes code may not work properly due to edited changes look into it

---
+++ HOUSE KEEPING PROJECT/javascript/main.js
@@ -0,0 +1,11 @@
+
+/*responsive top nav bar*/
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
